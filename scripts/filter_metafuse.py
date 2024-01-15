bc4fe56f74f9e9980d862e8b10e94c07996fb22b
patkarlab <nvpatkar@gmail.com>
2024-01-15 11:40:35 +0530

    filter metafuse script added

---
+++ scripts/filter_metafuse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# This script will filter the output of metafuse
+import sys
+import os
+import pandas as pd
+import openpyxl
+
+metafuse_input = sys.argv[1]
+metafuse_output = sys.argv[2]
+
+xl_file = pd.read_excel(metafuse_input, sheet_name=None)
+with pd.ExcelWriter(metafuse_output) as writer:
+	for sheet_names in xl_file.keys():
+		df = pd.read_excel(input_xlsx_file, sheet_name=sheet_names)
+		df.fillna(value= -1 , inplace=True)
+		if 'max_split_cnt' in df:
+			df = df[df.max_split_cnt > 5]
+		df.to_excel(writer, sheet_name=sheet_names, index=False)	
